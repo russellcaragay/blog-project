27ede4f056892e71aae725f69d26d82cb93c6259
russellcaragay <caragayrussell@gmail.com>
2023-01-22 23:44:36 +0800

    added function to get published post

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home",{homeInfo : homeStartingContent});
+  res.render("home",{homeInfo : homeStartingContent, publishedPost: postList});
 })
 
 app.get("/about",function(req,res){
